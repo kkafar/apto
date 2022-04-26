5e9c7c6a583a52679e4c1f3be6c0d8f2a810d5ee
Kacper Kafara <kacperkafara@gmail.com>
2022-04-26 23:18:41 +0200

    refact: cleanup the code a little bit

--- solution/src/main/cpp/main.cpp
+++ solution/src/main/cpp/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
 #include <queue>
 
 constexpr static int MaxBoardSize = 200;
@@ -34,17 +33,13 @@ void bfs(Config &config) {
   std::queue<std::pair<int, int>> queue;
   queue.push(std::move(config.startPoint));
 
-//  int i = 1;
   while (!queue.empty()) {
-//    std::cout << "Iteration: " << i++ << ' ';
     std::pair<int, int> currentPosition = std::move(queue.front());
-//    std::cout << "Considered position: " << currentPosition.first << " " << currentPosition.second << '\n';
     queue.pop();
 
     if (currentPosition.first == config.finishPoint.first && currentPosition.second == config.finishPoint.second) {
       std::pair<int, int> parent = std::move(config.board[currentPosition.first][currentPosition.second].parent);
       while (parent.first != -1) {
-//        std::cout << parent.first << " " << parent.second << '\n';
         if (parent.first > currentPosition.first) {
           config.solution.push_back('G');
         } else if (parent.first < currentPosition.first) {
@@ -64,7 +59,6 @@ void bfs(Config &config) {
 
     if (currentPosition.first + 1 < config.dims.height &&
         config.board[currentPosition.first + 1][currentPosition.second].status == FieldStatus::Empty) {
-//      std::cout << "\tAdding " << currentPosition.first + 1 << " " << currentPosition.second << '\n';
       queue.emplace(currentPosition.first + 1, currentPosition.second);
       config.board[currentPosition.first + 1][currentPosition.second].parent = currentPosition;
       config.board[currentPosition.first + 1][currentPosition.second].status = FieldStatus::Enqueued;
@@ -72,14 +66,12 @@ void bfs(Config &config) {
     }
     if (currentPosition.first - 1 >= 0 &&
         config.board[currentPosition.first - 1][currentPosition.second].status == FieldStatus::Empty) {
-//      std::cout << "\tAdding " << currentPosition.first - 1 << " " << currentPosition.second << '\n';
       queue.emplace(currentPosition.first - 1, currentPosition.second);
       config.board[currentPosition.first - 1][currentPosition.second].parent = currentPosition;
       config.board[currentPosition.first - 1][currentPosition.second].status = FieldStatus::Enqueued;
     }
     if (currentPosition.second + 1 < config.dims.width &&
         config.board[currentPosition.first][currentPosition.second + 1].status == FieldStatus::Empty) {
-//      std::cout << "\tAdding " << currentPosition.first << " " << currentPosition.second + 1 << '\n';
       queue.emplace(currentPosition.first, currentPosition.second + 1);
       config.board[currentPosition.first][currentPosition.second + 1].parent = currentPosition;
       config.board[currentPosition.first][currentPosition.second + 1].status = FieldStatus::Enqueued;
@@ -87,7 +79,6 @@ void bfs(Config &config) {
     if (currentPosition.second - 1 >= 0 &&
         config.board[currentPosition.first][currentPosition.second - 1].status == FieldStatus::Empty) {
       queue.emplace(currentPosition.first, currentPosition.second - 1);
-//      std::cout << "\tAdding " << currentPosition.first << " " << currentPosition.second - 1 << '\n';
       config.board[currentPosition.first][currentPosition.second - 1].parent = currentPosition;
       config.board[currentPosition.first][currentPosition.second - 1].status = FieldStatus::Enqueued;
     }
@@ -102,13 +93,9 @@ int main(int argc, char * argv[]) {
   loadInputFromStdIn(config);
   bfs(config);
 
-//  std::cout << "BFS ENDED\n";
-
-//  config.solution.shrink_to_fit();
-  for (std::vector<char>::reverse_iterator i = config.solution.rbegin(); i != config.solution.rend(); ++i) {
+  for (auto i = config.solution.rbegin(); i != config.solution.rend(); ++i) {
     std::cout << *i;
   }
-//  std::cout << '\n';
   return 0;
 }
 
@@ -123,28 +110,19 @@ void loadInputFromStdIn(Config &config) {
 
   // board
   for (int row = 0; row < config.dims.height; ++row) {
-//    std::cin.clear();
-//    std::cin.getline(lineBuffer, config.dims.width);
-//    printBuffer(lineBuffer, config.dims.width);
     std::cin >> lineBuffer;
-//    std::cout << lineBuffer << '\n';
     for (int col = 0; col < config.dims.width; ++col) {
       if (lineBuffer[col] == '.') {
         config.board[row][col].status = FieldStatus::Empty;
-//        std::cout << row << ' ' << col << " empty\n";
       } else if (lineBuffer[col] == '#') {
         config.board[row][col].status = FieldStatus::Blocked;
-//        std::cout << row << ' ' << col << " blocked\n";
       } else if (lineBuffer[col] >= 'a' && lineBuffer[col] <= 'z') {
         config.board[row][col].status = FieldStatus::Start;
         config.board[row][col].parent = {-1, -1};
         config.startPoint = {row, col};
-//        std::cout << row << ' ' << col << " start\n";
       } else if (lineBuffer[col] >= 'A' && lineBuffer[col] <= 'Z') {
-//        config.board[row][col].status = FieldStatus::Finish;
         config.board[row][col].status = FieldStatus::Empty;
         config.finishPoint = {row, col};
-//        std::cout << row << ' ' << col << " finish\n";
       }
     }
   }
